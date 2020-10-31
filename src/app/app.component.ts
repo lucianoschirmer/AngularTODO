e0329055ad3a2a02e13a05cb5ad0f67686d2b3fa
Luciano Schirmer <lucianoschirmerbrasil@gmail.com>
2020-10-31 20:03:35 -0300

    criado objeto todo e construtos para a class todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = [];
+  public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
 
   constructor() {
-    this.todos.push('passear com o cachorro');
-    this.todos.push('ir ao supermercado');
-    this.todos.push('cortar o cabelo');
+    this.todos.push(new Todo(1, 'passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'cortar o cabelo', false));
   }
 
   alteratexto() {
--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,3 +1,7 @@
 export class Todo {
-
+    constructor(
+        public id: Number,
+        public todo: String,
+        public done: Boolean,
+    ) { }
 }
