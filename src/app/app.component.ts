bdbf5e05a7283f3d206077ab902da23867214036
Luciano Schirmer <lucianoschirmerbrasil@gmail.com>
2020-11-03 07:14:38 -0300

    criado metodo para salvar no localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,20 +21,32 @@ export class AppComponent {
       ])]
     });
 
-    this.todos.push(new Todo(1, 'passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'cortar o cabelo', false));
+    // this.todos.push(new Todo(1, 'passear com o cachorro', false));
+    // this.todos.push(new Todo(2, 'ir ao supermercado', false));
+    // this.todos.push(new Todo(3, 'cortar o cabelo', false));
+
+    this.load();
   }
 
   add() {
+    // this.form.value => { title: 'Título' };
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
 
+    this.save();
+    this.clear();
   }
+
+  clear() {
+    this.form.reset();
+  }
+
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
-
   }
 
   markAsDone(todo: Todo) {
@@ -43,6 +55,16 @@ export class AppComponent {
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data);
   }
 
 }
